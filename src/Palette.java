75a54409f628fc488d6de6ce86e329f5725ee905
Jason Cameron <git@jasoncameron.dev>
2025-05-22 14:55:10 -0400

    Add Palette class for custom color management

---
+++ src/Palette.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Palette extends Color {
+    public static final Color ZColor =  new Color(0xB3CEE5);
+
+    public Palette(int r, int g, int b) {
+        super(r, g, b);
+    }
+}
