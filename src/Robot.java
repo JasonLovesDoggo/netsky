c63d27558f03473c53c942acf5854517d1abb360
destrier2 <zoe.li@student.tdsb.on.ca>
2025-05-22 15:15:43 -0400

    Robot

--- src/Robot.java
+++ src/Robot.java
@@ -1,17 +1,26 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 public class Robot{
 	public int x;
 	public int y;
+	public int direction;
 	
-	/*public void paint(Graphics g) {
+	public void drawRobot(Graphics g, Graphics2D g2) {
+		Image robot = new ImageIcon("./Images/robot.png").getImage();
+		robot = robot.getScaledInstance((direction) * robot.getWidth(null), robot.getHeight(null), Image.SCALE_DEFAULT);
+		g.drawImage(robot, x, y, robot.getWidth(null), robot.getHeight(null), null);
 		
-		
-	}*/
-	
-	public void drawRobot(Graphics g) {
-		g.setColor(Color.white);
+		/*g.setColor(Color.white);
 		g.fillOval(x-(72/2), y-(111/2), 72, 111);
+		
+		
+		g.setColor(new Color(0xb3cee5));
+		Path2D curve = new Path2D.Double();
+		curve.moveTo(x-36, y-8.4);
+		curve.curveTo(x-36, y-8.4, x, y+30, x+36, y+11);
+		g2.draw(curve);*/
 	}
+	
 }
